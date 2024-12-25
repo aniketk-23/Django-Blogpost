0e3b2f3db77dfd4699efe11e2a75c117de8316dd
aniketk-23 <aniketkumarbgs2004@gmail.com>
2024-12-25 15:44:19 +0530

    Update Post and date_format

--- blog/views.py
+++ blog/views.py
@@ -2,26 +2,27 @@ from lib2to3.fixes.fix_input import context
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author': 'Aniket Kumar',
-        'title': 'Blog Post 1',
-        'content': 'First Post Content',
-        'date_posted': 'Dec 23, 2024'
-    },
-    {
-        'author': 'Kartik',
-        'title': 'Blog Post 2',
-        'content': 'Second Post Content',
-        'date_posted': 'Dec 24, 2024'
-    }
-
-]
+# posts = [
+#     {
+#         'author': 'Aniket Kumar',
+#         'title': 'Blog Post 1',
+#         'content': 'First Post Content',
+#         'date_posted': 'Dec 23, 2024'
+#     },
+#     {
+#         'author': 'Kartik',
+#         'title': 'Blog Post 2',
+#         'content': 'Second Post Content',
+#         'date_posted': 'Dec 24, 2024'
+#     }
+#
+# ]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
